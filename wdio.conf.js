1e3973ed8d08b15c2d8abca61ff7603881dbb77f
Enrique Gonzalez <enrique@saucelabs.com>
2020-10-07 15:40:25 -0700

    retry once if spec fails

--- wdio.conf.js
+++ wdio.conf.js
@@ -138,7 +138,7 @@ exports.config = {
     framework: 'mocha',
     //
     // The number of times to retry the entire specfile when it fails as a whole
-    // specFileRetries: 1,
+    specFileRetries: 1,
     //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
